0b95127bec3cc1a78415d01a1400ed7f3b182ed9
Pavel Goltsev <p.goltsev@gmail.com>
2017-05-29 18:57:51 +0300

    Adjusted Quicksort implementations.

--- python/algorithms/sorting/quicksort.py
+++ python/algorithms/sorting/quicksort.py
@@ -1,58 +1,36 @@
-import copy
-
-
-def quicksort1(arr):
+def quicksort1(arr, start_idx=None, end_idx=None):
     """
     Sort array using Quicksort algorithm.
+    Classical algorithm was introduced by C. A. R. Hoare.
     :param arr: Array to sort.
+    :param start_idx: Start index. By default is zero.
+    :param end_idx: End index. By default is the last index of array.
     :return: Sorted array.
-
-    >>> quicksort1([10, 2, 4, 6, 7, 5, 3, 1, 9, 8])
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    >>> quicksort1([10, 2])
-    [2, 10]
-    >>> quicksort1([1, 2])
-    [1, 2]
-    >>> quicksort1([2])
-    [2]
-    >>> quicksort1([])
-    []
     """
+    if start_idx is None:
+        start_idx = 0
+    if end_idx is None:
+        end_idx = len(arr) - 1
 
-    if len(arr) < 2:
-        return arr
-
-    result = copy.copy(arr)
-    pivot_index = len(result) - 1
-    cur_ind = 0
-    while cur_ind < pivot_index and pivot_index >= 0:
-        pivot = result[pivot_index]
-        cur_val = result[cur_ind]
-        if cur_val > pivot:
-            result.insert(pivot_index, result.pop(cur_ind))
-            pivot_index -= 1
-        else:
-            cur_ind += 1
-
-    result = quicksort1(result[:pivot_index]) + [pivot] + quicksort1(
-        result[pivot_index + 1:])
-
-    return result
+    if start_idx < end_idx:
+        part_index = partition1(arr, start_idx, end_idx)
+        quicksort1(arr, start_idx, part_index)
+        quicksort1(arr, part_index + 1, end_idx)
 
 
-def partition2(arr, start_ind, end_index):
-    pivot = arr[start_ind]
-    i = start_ind - 1
-    j = end_index + 1
+def partition1(arr, start_idx, end_idx):
+    pivot = arr[start_idx]
+    i = start_idx - 1
+    j = end_idx + 1
     while True:
         while True:
             i = i + 1
-            if arr[i] < pivot:
+            if arr[i] >= pivot:
                 break
 
         while True:
             j = j - 1
-            if pivot < arr[j]:
+            if arr[j] <= pivot:
                 break
 
         if i >= j:
@@ -61,79 +39,38 @@ def partition2(arr, start_ind, end_index):
         arr[i], arr[j] = arr[j], arr[i]
 
 
-def quicksort2(arr, start_ind=None, end_index=None):
+def quicksort2(arr, start_idx=None, end_idx=None):
     """
     Sort array using Quicksort algorithm.
-    Classical algorithm introduced by C. A. R. Hoare.
+    Algorithm was introduced by Nico Lomuto.
     :param arr: Array to sort.
-    :param start_ind: Start index. By default is zero.
-    :param end_ind: End index. By default is the last index of array.
+    :param start_idx: Start index. By default is zero.
+    :param end_idx: End index. By default is the last index of array.
     :return: Sorted array.
-
-    >>> quicksort1([10, 2, 4, 6, 7, 5, 3, 1, 9, 8])
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    >>> quicksort1([10, 2])
-    [2, 10]
-    >>> quicksort1([1, 2])
-    [1, 2]
-    >>> quicksort1([2])
-    [2]
-    >>> quicksort1([])
-    []
     """
-    if start_ind is None:
-        start_ind = 0
-    if end_index is None:
-        end_index = len(arr) - 1
+    if start_idx is None:
+        start_idx = 0
+    if end_idx is None:
+        end_idx = len(arr) - 1
 
-    if start_ind < end_index:
-        part_index = partition2(arr, start_ind, end_index)
-        quicksort2(arr, start_ind, part_index)
-        quicksort2(arr, part_index + 1, end_index)
+    if start_idx < end_idx:
+        part_index = partition2(arr, start_idx, end_idx)
+        quicksort2(arr, start_idx, part_index - 1)
+        quicksort2(arr, part_index + 1, end_idx)
 
 
-def partition3(arr, start_ind, end_index):
-    pivot = arr[end_index]
-    i = start_ind - 1
-    for j in range(end_index - 1):
+def partition2(arr, start_idx, end_idx):
+    pivot = arr[end_idx]
+    i = start_idx - 1
+    j = start_idx
+    while j <= end_idx - 1:
         if arr[j] <= pivot:
             i += 1
             if i != j:
                 arr[i], arr[j] = arr[j], arr[i]
-
-    arr[i + 1], arr[end_index] = arr[end_index], arr[i + 1]
+        j += 1
+    arr[i + 1], arr[end_idx] = arr[end_idx], arr[i + 1]
     return i + 1
-
-
-def quicksort3(arr, start_ind=None, end_index=None):
-    """
-    Sort array using Quicksort algorithm.
-    Algorithm introduced by Nico Lomuto.
-    :param arr: Array to sort.
-    :param start_ind: Start index. By default is zero.
-    :param end_ind: End index. By default is the last index of array.
-    :return: Sorted array.
-
-    >>> quicksort1([10, 2, 4, 6, 7, 5, 3, 1, 9, 8])
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    >>> quicksort1([10, 2])
-    [2, 10]
-    >>> quicksort1([1, 2])
-    [1, 2]
-    >>> quicksort1([2])
-    [2]
-    >>> quicksort1([])
-    []
-    """
-    if start_ind is None:
-        start_ind = 0
-    if end_index is None:
-        end_index = len(arr) - 1
-
-    if start_ind < end_index:
-        part_index = partition3(arr, start_ind, end_index)
-        quicksort2(arr, start_ind, part_index - 1)
-        quicksort2(arr, part_index + 1, end_index)
 
 
 if __name__ == '__main__':
---
+++ python/algorithms/sorting/test_quicksort.py
@@ -0,0 +1,77 @@
+from unittest import TestCase
+
+from python.algorithms.sorting.quicksort import quicksort1, quicksort2
+
+
+class QuickSort1TestCase(TestCase):
+    def test_common_array(self):
+        arr = [10, 2, 4, 6, 7, 5, 3, 1, 9, 8]
+
+        quicksort1(arr)
+
+        self.assertListEqual(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    def test_only_two_elements(self):
+        arr = [10, 2]
+
+        quicksort1(arr)
+
+        self.assertListEqual(arr, [2, 10])
+
+    def test_ordered(self):
+        arr = [1, 2]
+
+        quicksort1(arr)
+
+        self.assertListEqual(arr, [1, 2])
+
+    def test_one_element(self):
+        arr = [2]
+
+        quicksort1(arr)
+
+        self.assertListEqual(arr, [2])
+
+    def test_no_elements(self):
+        arr = []
+
+        quicksort1(arr)
+
+        self.assertListEqual(arr, [])
+
+
+class QuickSort2TestCase(TestCase):
+    def test_common_array(self):
+        arr = [10, 2, 4, 6, 7, 5, 3, 1, 9, 8]
+
+        quicksort2(arr)
+
+        self.assertListEqual(arr, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    def test_only_two_elements(self):
+        arr = [10, 2]
+
+        quicksort2(arr)
+
+        self.assertListEqual(arr, [2, 10])
+
+    def test_ordered(self):
+        arr = [1, 2]
+
+        quicksort2(arr)
+
+        self.assertListEqual(arr, [1, 2])
+
+    def test_one_element(self):
+        arr = [2]
+
+        quicksort2(arr)
+
+        self.assertListEqual(arr, [2])
+
+    def test_no_elements(self):
+        arr = []
+
+        quicksort2(arr)
+
+        self.assertListEqual(arr, [])
